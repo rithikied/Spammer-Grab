01185e1b0962c40f91e8fef1ac0c20e3115b9765
MisterNay <misternay@odds.team>
2018-04-24 15:38:46 +0700

    Fix User-Agent STATUS 403

--- spammer.py
+++ spammer.py
@@ -12,11 +12,14 @@
 # | - Added support for phone number with +xxxxxx format 
 # | - And another fixes
 
-import spammer_class
-spammer = spammer_class.Spammer()
+import spammer_decompile
+spammer = spammer_decompile.Spammer()
+spamer = spammer_decompile
 spammer.author = "P4kL0nc4t"
 try:
     spammer.main()
 except KeyboardInterrupt:
-    print spammer_class.color.FAIL+spammer_class.color.REVERSE+"\r[!][except] KeyboardInterrupt detected! Exiting . . ."+spammer_class.color.ENDC
+    print(spamer.color.FAIL + spamer.color.REVERSE +\
+          "\r[!][except] KeyboardInterrupt detected! Exiting . . ." \
+          + spamer.color.ENDC)
     exit()
